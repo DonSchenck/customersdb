1d2ad3c6cf9e33569ff212f738a51e219f00908a
Don Schenck <don.schenck@gmail.com>
2023-10-06 09:49:47 -0400

    Added comments to scripts

--- create_and_load.ps1
+++ create_and_load.ps1
@@ -1,11 +1,24 @@
-(kubectl get pods | select-string '^customersdb([^\s]+)-(?!deploy)') -match 'customersdb([^\s]+)'; $podname = $matches[0]
 
+echo 'CREATE AND POPULATE DATABASE TABLE(S)'
+echo '-------------------------------------'
+
+echo 'Waiting for pod to be available...'
+Sleep(15)
+
+echo 'Getting pod name...'
+(kubectl get pods | select-string '^customersdb([^\s]+)-(?!deploy)') -match 'customersdb([^\s]+)'; $podname = $matches[0]
 Write-Host $podname
 
+echo 'Copying table-creation script to pod...'
 oc cp .\create_table_customers.sql ${podname}:/tmp/create_table_customers.sql
 
+echo 'Copying data to pod...'
 oc cp .\customers.csv ${podname}:/tmp/customers.csv
 
+echo 'Creating table(s)...'
 oc exec ${podname} -- psql -d customersdb -U customersdb --no-password -f "/tmp/create_table_customers.sql"
 
+echo 'Importing data...'
 oc exec ${podname} -- psql -d customersdb -U customersdb -c "\copy customers FROM '/tmp/customers.csv' delimiter ',' csv"
+
+echo 'FINISHED'
--- create_and_load.sh
+++ create_and_load.sh
@@ -1,10 +1,23 @@
-export PODNAME=$(oc get pods -o custom-columns=POD:.metadata.name --no-headers | grep -v 'deploy$' | grep quote)
+echo 'CREATE AND POPULATE DATABASE TABLE(S)'
+echo '-------------------------------------'
+
+echo 'Waiting for pod to be available...'
+Sleep 15
+
+echo 'Getting pod name...'
+export PODNAME=$(oc get pods -o custom-columns=POD:.metadata.name --no-headers | grep -v 'deploy$' | grep customersdb)
 echo $PODNAME
 
-oc cp .\create_table_customers.sql $PODNAME:/tmp/create_table_customers.sql
+echo 'Copying table-creation script to pod...'
+oc cp create_table_customers.sql $PODNAME:/tmp/create_table_customers.sql
 
-oc cp .\customers.csv $PODNAME:/tmp/customers.csv
+echo 'Copying data to pod...'
+oc cp customers.csv $PODNAME:/tmp/customers.csv
 
+echo 'Creating table(s)...'
 oc exec $PODNAME -- psql -d customersdb -U customersdb --no-password -f "/tmp/create_table_customers.sql"
 
+echo 'Importing data...'
 oc exec $PODNAME -- psql -d customersdb -U customersdb -c "\copy customers FROM '/tmp/customers.csv' delimiter ',' csv"
+
+echo 'FINISHED'
